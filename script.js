a8242fb66e22586b5adb94015c01efbc841ff4bf
pbullock08 <126013657+pbullock08@users.noreply.github.com>
2023-04-04 18:29:44 -0400

    added comments to better conceptualize the acceptance criteria and finalized variables, flow of code, and if/else statements

--- script.js
+++ script.js
@@ -1,49 +1,55 @@
 // Assignment code here
-//character variables 
+
 function generatePassword (){
-var uppercase = ["A", "B", "C", "D", "E"];
-var lowercase = ["v", "w", "x", "y", "z"];
-var numerics = [2,4,6,8,0];
-var symbols = ["!", "@", "#", "$", "%"];
+//create a bank of variables that will randomly generate characters depending on criteria and a pool that combines them together into 1 array
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numerics = [1,2,3,4,5,6,7,8,9,0];
+var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_"];
 var characters = uppercase.concat(lowercase, numerics, symbols);
 
-//add event listener for generate password to initiate prompts 
+//code that leads to prompts when you first click generate password 
+var generateBtn = document.querySelector("#generate");
 
-//code for prompts
-var numofCharacters = parseInt(prompt("How many characters should your password contain?"));
-if (numofCharacters >=8 && numofCharacters <=128){
-  
-} else {alert("Choose a number between 8 and 128.")}
+//generateBtn.addEventListener("click", ???());
 
+//create a prompt and if/else statement for number of characters between 8 and 128
+var numofCharacters = parseInt(prompt("How many characters (between 8 and 128) should your password contain?"));
+
+if (numofCharacters >=8 && numofCharacters <=128) {
+  var uppercasePrompt = confirm("Should your password include uppercase letters?");
+} else {
+  alert("Your password must be between 8 and 128 characters.");
+  var numofCharacters = parseInt(prompt("How many characters (between 8 and 128) should your password contain?"));
+} //how do you make this repeat -- a loop?
+
+//create prompts and if/else statement for selection of at least one type of character using or ||
 var uppercasePrompt = confirm("Should your password include uppercase letters?");
 var lowercasePrompt = confirm("Should your password include lowercase letters?");
 var numericsPrompt = confirm("Should your password include numerical values?");
 var symbolsPrompt = confirm("Should your password include special characters?");
 
-//code for generated password from user inputs
+if (uppercasePrompt || lowercasePrompt || numericsPrompt || symbolsPrompt) {
+  //generate password 
+} else { 
+  alert("Your password must contain at lease one type of character.");
+  var uppercasePrompt = confirm("Should your password include uppercase letters?");
+  var lowercasePrompt = confirm("Should your password include lowercase letters?");
+  var numericsPrompt = confirm("Should your password include numerical values?");
+  var symbolsPrompt = confirm("Should your password include special characters?");
+}
+}
+//generate password using a for loop that considers criteria and variable pool 
 var password = [];
+
 for (var i=0; i < numofCharacters; i++) {
   (characters[Math.floor(Math.random() * numofCharacters)]);
 }
+//}
+//generatePassword ();
+//}
+//display password in the your secure password box 
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
 
-
-
-// Get references to the #generate element - button
-var generateBtn = document.querySelector("#generate");
-//var generateBtn = document.querySelector("#generate").addEventListener("click", charactersPrompt());
-//function charactersPrompt () {
-
-
-// Write password to the #password input -- output your passwod on the screen
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-console.log (password);
-generatePassword ();
-}
+passwordText.value = password;
