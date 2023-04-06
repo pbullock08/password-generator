5dea1372a52057262c6b1fd9356023391373527a
pbullock08 <126013657+pbullock08@users.noreply.github.com>
2023-04-06 17:41:52 -0400

    fixed issue with code accepting decimals and letters for number of characters prompt

--- script.js
+++ script.js
@@ -17,11 +17,11 @@ function generatePassword() {
   var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var numerics = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0,1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-  var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "?", "~"];
+  var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "?", "~", "<", ">"];
   //prompt for # of characters 
-  var numofCharacters = parseInt(prompt("How many characters (between 8 and 128) should your password contain?"));
+  var numofCharacters = (prompt("How many characters (between 8 and 128) should your password contain?"));
 
-  if (numofCharacters < 8 || numofCharacters > 128 === true) {
+  if (numofCharacters < 8 || numofCharacters > 128 || !Number.isInteger(+numofCharacters) === true) {
     alert("Your password must be between 8 and 128 characters.");
   } else {
     //prompts for types of characters
@@ -30,6 +30,7 @@ function generatePassword() {
     var numericsPrompt = confirm("Should your password include numerical values?");
     var symbolsPrompt = confirm("Should your password include special characters?");
   }
+
   //if statements for selection of at least one type of character
   if (uppercasePrompt === false && lowercasePrompt === false && numericsPrompt === false && symbolsPrompt === false) {
     alert("Your password must contain at lease one type of character.");
